2b65d494c3fb0094013fc66860a32b73de7455f4
webjen <jryden@outlook.com>
2012-09-04 21:01:11 +0200

    Rearranged link to chmod-post

--- report.php
+++ report.php
@@ -10,10 +10,10 @@
 
   <h2>Reflektioner</h2>
   <p>Hade inga större problem med denna första uppgift. Den var tydlig, pedagogisk och lätt att följa. Lite meckigt var det att 
-    behöva chmod:a varje gång man för över en ny fil eller mapp, men så hittade jag ett inlägg om just detta här på bloggen och vips 
-    så var det problemet löst: 
-    <a href="http://db-o-webb.blogspot.se/2009/09/sshstudentbthse-editera-initfilen-vid.html" target="_blank">
-      http://db-o-webb.blogspot.se/2009/09/sshstudentbthse-editera-initfilen-vid.html</a></p>
+    behöva chmod:a varje gång man för över en ny fil eller mapp, men så hittade jag ett 
+    <a href="http://db-o-webb.blogspot.se/2009/09/sshstudentbthse-editera-initfilen-vid.html" target="_blank">inlägg</a> om just detta här på bloggen och vips 
+    så var det problemet löst.
+    </p>
   <p>Det var också lite trixigt att hitta inställningarna för Unix-radbrytningar i den editor jag använder, men till slut gick det.
     Den var redan inställd på UTF-8, så det behövde jag inte ändra.</p>
   <p>Valideringen gick bra, fick grön skylt med några varningar för CSS:en. Främst gällde de färgerna men fick även en om att 
