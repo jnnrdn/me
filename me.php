befe968d7175d6c7a05b8857d5596b4da7a5e65c
webjen <jryden@outlook.com>
2012-09-04 15:52:45 +0200

    Added real text to the me-page and floated image Wanted to have the image float to the left so put a class on the photo of me and floated it. Cleared the hr

--- me.php
+++ me.php
@@ -7,11 +7,15 @@
 <?php include("incl/header.php"); ?>
 
 	<h1>htmlphp-me</h1>
-
-	<p>Här kommer snart min egen fina me-sida. Jag ändrar lite för att testa git. Testar lite till. Och lite till...</p>
-	<p>Gör en ny ändring</p>
-
-	<img src="img/me.jpg" alt="Bild på Jenny Ryden">
+	
+	<img src="img/me.jpg" alt="Bild på Jenny Ryden" class="me">
+	
+	<p>Jag heter Jenny Rydén och är 37 år. Bor med min sambo i Göteborg och jobbar som webbutvecklare. </p>
+	<p>Idag är jag mest hemma på frontend-sidan men jag har lite kunskaper i PHP sen innan och tycker det är jättekul, 
+		så med det här kurspaketet hoppas jag bredda och fördjupa dessa. Är även mycket intresserad av design och användbarhet.</p>
+	<p>Älskar att lära mig nya saker så vidareutbildning (helst inom ovanstående områden) kan nog sägas vara min största hobby. </p>
+	<p>I övrigt är jag nog som de flesta andra: gillar att läsa, djur och natur samt att umgås med nära och kära.</p>
+	
 	<hr>
  
 <?php include("incl/footer.php"); ?>
