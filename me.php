bd063531b7f93ff6e6fd587e6ec7ec9c0bff1722
webjen <jryden@outlook.com>
2012-09-04 09:35:00 +0200

    First commit of BTH project

---
+++ me.php
@@ -0,0 +1,16 @@
+<?php include("incl/config.php"); ?>
+
+<?php 
+	$title = "Min Me-sida om mig själv"; 
+?>
+
+<?php include("incl/header.php"); ?>
+
+	<h1>htmlphp-me</h1>
+  <p>Här kommer snart min egen fina me-sida.</p>
+  <img src="img/me.jpg" alt="Bild på Jenny Ryden">
+  <hr>
+ 
+<?php include("incl/footer.php"); ?>
+
+
---
+++ report.php
@@ -0,0 +1,15 @@
+<?php include("incl/config.php"); ?>
+
+<?php
+	$title = "Mina redovisningar av kursmomenten";
+?>
+
+<?php include("incl/header.php"); ?>
+
+  <h1>Redovisning av kursmomenten</h1>
+  <p>Här kommer snart sidan för redovisning av de olika kursmomenten.</p>
+  <hr>
+ 
+<?php include("incl/footer.php"); ?>
+
+
