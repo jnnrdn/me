6c318efc0af6bfc29f855b3cfc942c3459d90cc4
rydium <user@server>
2012-09-04 13:04:28 +0200

    first commit from mac

---
+++ me.php
@@ -0,0 +1,18 @@
+<?php include("incl/config.php"); ?>
+
+<?php 
+	$title = "Min Me-sida om mig själv"; 
+?>
+
+<?php include("incl/header.php"); ?>
+
+	<h1>htmlphp-me</h1>
+
+  <p>Här kommer snart min egen fina me-sida. Jag ändrar lite för att testa git. Testar lite till</p>
+
+  <img src="img/me.jpg" alt="Bild på Jenny Ryden">
+  <hr>
+ 
+<?php include("incl/footer.php"); ?>
+
+
