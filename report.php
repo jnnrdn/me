9f0cf1bc5423afc1a1a29a1dd3e6b83b4a89fa89
webjen <jryden@outlook.com>
2012-09-04 20:19:24 +0200

    Added more text to report page

--- report.php
+++ report.php
@@ -8,19 +8,26 @@
 
   <h1>Kmom01: Kom igång med HTML, CSS och PHP</h1>
 
-  <p>Hade inga större problem med denna första uppgift, den var tydlig, pedagogisk och det var lätt att följa instruktionerna. 
-    Det var inte så mycket nytt för min del, men vissa saker är det alltid bra att bli påmind om. Lite störigt var det att 
-    behöva ändra filrättigheterna varje gång man förde över en ny fil, men det är ju inte så mycket att göra åt. 
-    Det var också lite trixigt att hitta inställningarna för Unix-radbrytningar i den editor jag använder, men till slut gick det.</p>
-  <p>Valideringen gick bra, fick grön skylt men några varningar för CSS:en. Fick ett antal varningar om färgerna men de gjorde jag 
-    inget åt. Fick dock en varning om att type-attributet saknades i länken till CSS:en från me.php så lade till det.</p>
-  <p>Som editor har jag kört NetBeans ett bra tag, men har nyligen bytt till Sublime Text 2 som jag gillar skarpt. 
-    Sitter i första hand på Mac (OS X v10.6.8) när jag jobbar med uppgifterna i den här kursen, och min lokala webbserver 
-    är en LAMP-stack som jag kör via VirtualBox.</p>
-  <p>Webbläsare är vanligtvis i första hand Chrome, men FF funkar också. Har 
-    även de övriga vanliga installerade för test (FF, Opera, Safari). Min sambos windows-dator får tjänstgöra som IE-testmiljö. 
-    Som filöverföringsprogram använder jag FileZilla. Kan inte klara mig utan versionshantering numera, så Git har också en 
-    given plats i min verktygslåda.</p>
+  <h2>Reflektioner</h2>
+  <p>Hade inga större problem med denna första uppgift. Den var tydlig, pedagogisk och lätt att följa. Lite meckigt var det att 
+    behöva chmod:a varje gång man för över en ny fil eller mapp, men så hittade jag en käck bloggpost om detta problem och vips 
+    så var det problemet löst: 
+    <a href="http://db-o-webb.blogspot.se/2009/09/sshstudentbthse-editera-initfilen-vid.html" target="_blank">
+      http://db-o-webb.blogspot.se/2009/09/sshstudentbthse-editera-initfilen-vid.html</a></p>
+  <p>Det var också lite trixigt att hitta inställningarna för Unix-radbrytningar i den editor jag använder, men till slut gick det.
+    Den var redan inställd på UTF-8, så det behövde jag inte ändra.</p>
+  <p>Valideringen gick bra, fick grön skylt med några varningar för CSS:en. Främst gällde de färgerna men fick även en om att 
+    type-attributet saknades i länken till CSS:en från me.php så lade till det då det kändes som en simpel åtgärd.</p>
+  
+  <h2>Min miljö</h2>
+  <p>Den editor jag använder i första hand är Sublime Text 2, men det händer även att jag jobbar i NetBeans.
+    Sitter i första hand på Mac (OS X 10.6.8) när jag jobbar med uppgifterna i den här kursen, men det kommer nog även hända att jag 
+    jobbar från en Linux-dator (Ubuntu). Min lokala webbserver (båda datorerna) är en LAMP-stack som jag kör via VirtualBox.</p>
+  <p>Webbläsare är vanligtvis i första hand Chrome, men FF funkar också. Har även de övriga vanliga installerade för test 
+    (FF, Opera, Safari). Min gamla windows-dator (Win 7) får tjänstgöra som IE-testmiljö. Som filöverföringsprogram använder jag 
+    FileZilla eller Cyberduck. </p>
+  <p>Kan inte klara mig utan versionshantering numera, särskilt inte som det händer att jag jobbar från olika datorer, 
+    så Git har också en given plats i min verktygslåda.</p>
   <hr>
 
 <?php include("incl/footer.php"); ?>
