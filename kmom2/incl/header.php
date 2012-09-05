7f6659c41489a6e5ae65f0038a9554031fb3bb78
webjen <jryden@outlook.com>
2012-09-05 21:19:40 +0200

    Added header tag to header.php

--- kmom2/incl/header.php
+++ kmom2/incl/header.php
@@ -7,11 +7,13 @@
 </head>
 <body>
 	<!-- Header -->
-	<div id="top">
-			<img src="img/logo.png" alt="htmlphp logo" width=300 height=70>
-	</div>
-	<!-- Navigeringsmeny -->
-	<nav class="navmenu">
-			<a href="me.php">Me</a>
-			<a href="report.php">Redovisning</a>
-	</nav>
+  <header>
+  	<div id="top">
+  			<img src="img/logo.png" alt="htmlphp logo" width=300 height=70>
+  	</div>
+  	<!-- Navigeringsmeny -->
+  	<nav class="navmenu">
+  			<a href="me.php">Me</a>
+  			<a href="report.php">Redovisning</a>
+  	</nav>
+  </header>
