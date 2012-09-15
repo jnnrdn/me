7da31863fe6989700241a1efd0d2869cbd3b19f9
webjen <jryden@outlook.com>
2012-09-15 17:44:02 +0200

    Create a new copy of kmom2 as a base for kmom3

---
+++ kmom3/incl/byline.php
@@ -0,0 +1,6 @@
+  <!-- byline -->
+  <footer class="byline">
+    <figure class="right top"><img src="img/back.png" alt="Hitta en bild" height="50"></figure>
+    <p>/jenny</p>
+    <p>Jenny Rydén jobbar som webbutvecklare, älskar webben och gillar även grafisk design och användbarhet.</p>
+ </footer>
