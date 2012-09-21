60b622480094cb20ed3467d81ce9fbc7f976b6a9
webjen <jryden@outlook.com>
2012-09-21 20:48:20 +0200

    Change byline to Michael Jordan quote

--- kmom3/incl/byline.php
+++ kmom3/incl/byline.php
@@ -1,6 +1,7 @@
   <!-- byline -->
   <footer class="byline">
     <figure class="right top"><img src="img/back.png" alt="Hitta en bild" height="50"></figure>
-    <p>/jenny</p>
-    <p>Jenny Rydén jobbar som webbutvecklare, älskar webben och gillar även grafisk design och användbarhet.</p>
+    <p>"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times, I’ve been trusted 
+      to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.”
+      (Michael Jordan)</p>
  </footer>
