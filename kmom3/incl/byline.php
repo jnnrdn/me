9e72594efeb56c714160e55e1d0a649e5435fa77
webjen <jryden@outlook.com>
2012-09-22 20:21:39 +0200

    Style footer

--- kmom3/incl/byline.php
+++ kmom3/incl/byline.php
@@ -1,7 +1,7 @@
   <!-- byline -->
   <footer class="byline">
     <figure class="right top"><img src="img/back.png" alt="Hitta en bild" height="50"></figure>
-    <p>"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times, I’ve been trusted 
+    <blockquote>"I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times, I’ve been trusted 
       to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.”
-      (Michael Jordan)</p>
+      (Michael Jordan)</blockquote>
  </footer>
