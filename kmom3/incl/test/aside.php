ac05ff0227f3148886c49a519a19fc5f548c59e5
webjen <jryden@outlook.com>
2012-09-22 22:15:29 +0200

    Rename menu links for test pages

--- kmom3/incl/test/aside.php
+++ kmom3/incl/test/aside.php
@@ -1,16 +1,16 @@
 <nav id="left-menu" class="left">
   <h3>kmom2</h3>
     <ul>
-      <li><a href="test.php?p=kmom02_pagestyle">kmom02_pagestyle.php</a></li>
+      <li><a href="test.php?p=kmom02_pagestyle">Sidspecifik style</a></li>
     </ul>
 
   <h3>kmom3</h3>
   <ul>
-      <li><a href="test.php?p=kmom03_get">kmom03_get.php</a></li>
-      <li><a href="test.php?p=kmom03_getform">kmom03_getform.php</a></li>
-      <li><a href="test.php?p=kmom03_postform">kmom03_postform.php</a></li>
-      <li><a href="test.php?p=kmom03_validate">kmom03_validate.php</a></li>
-      <li><a href="test.php?p=kmom03_server">kmom03_server.php</a></li>
+      <li><a href="test.php?p=kmom03_get">Visa innehållet i GET</a></li>
+      <li><a href="test.php?p=kmom03_getform">Testa formulär och GET</a></li>
+      <li><a href="test.php?p=kmom03_postform">Testa formulär och POST</a></li>
+      <li><a href="test.php?p=kmom03_validate">Validera inmatning</a></li>
+      <li><a href="test.php?p=kmom03_server">Kolla innehåll i SERVER</a></li>
       <li><a href="test.php?p=kmom03_sessiondestroy">Förstör sessionen</a></li>
   </ul>
 
