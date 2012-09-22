f167fa61acd3c9619f984690c99dcf48ac9a9f0b
webjen <jryden@outlook.com>
2012-09-22 21:08:40 +0200

    Add lang attribute and session start

--- kmom3/incl/config.php
+++ kmom3/incl/config.php
@@ -1,3 +1,8 @@
 <?php
+// start a named session
+session_name(preg_replace('/[:\.\/-_]/', '', __FILE__));
+session_start();
+
 include_once(dirname(__FILE__) . '/../src/common.php');
+
 error_reporting(-1);
