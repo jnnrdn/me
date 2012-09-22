a93c1b70e83055ae21569c6337c88710e48f0162
webjen <jryden@outlook.com>
2012-09-22 19:18:03 +0200

    Add more links to tools to footer and adjusted link to viewsource on default.php

--- kmom3/incl/footer.php
+++ kmom3/incl/footer.php
@@ -7,7 +7,9 @@
         <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
         <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">CSS3</a>
         <a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance">Unicorn</a>
-        <a href="source.php">Källkod</a>
+        <a href="http://validator.w3.org/i18n-checker/check?uri=<?php echo getCurrentUrl(); ?>">i18n</a>
+        <a href="http://validator.w3.org/checklink?uri=<?php echo getCurrentUrl(); ?>">Links</a>
+        <a href="viewsource.php">Källkod</a>
       </p>
       <p>Manualer:
         <a href="http://www.w3.org/2009/cheatsheet/">Cheatsheet</a>
@@ -20,6 +22,9 @@
         <a href="http://dbwebb.se/htmlphp/me/kmom02/guide.php?id=html20">HTML-20</a>
         <a href="http://dbwebb.se/htmlphp/me/kmom02/guide.php?id=css20">CSS-20</a> 
       </p>
+      <?php if(isset($pageTimeGeneration)) : ?>
+        <p>Page generated in <?php echo round(microtime(true)-$pageTimeGeneration, 5); ?> seconds</p>
+      <?php endif; ?>
     </footer>
   </div>
 </body>        
--- kmom3/incl/test/default.php
+++ kmom3/incl/test/default.php
@@ -5,7 +5,7 @@
 med de testfall och exempel som kodas i de olika kursmomenten.</p>
 
 <p>Källkoden för den dynamiska sidan, <code>test_minimal.php</code>, 
-<a href="viewsource.php?dir=&file=test_minimal.php">hittar du här</a>.</p>
+<a href="viewsource.php?dir=&file=test.php">hittar du här</a>.</p>
 
 <p>Källkoden till mina testfall, och övriga delar till sidan, 
 <a href="http://dbwebb.se/htmlphp/kmom03/me/viewsource.php?dir=incl/test">hittar du i denna katalogen</a>.</p>
