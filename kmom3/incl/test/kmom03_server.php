6e79c2f367eeb9c37a60a184c94ea74e09f823a8
webjen <jryden@outlook.com>
2012-09-23 08:31:41 +0200

    Fix styling and markup on code boxes

--- kmom3/incl/test/kmom03_server.php
+++ kmom3/incl/test/kmom03_server.php
@@ -1,13 +1,10 @@
 <h1>Formulär och post-metoden</h1>
- <p>Länken till denna sida:</p>
- <a href="<?php echo getCurrentUrl();?>"><?php echo getCurrentUrl();?></a>
- <pre>
+  <p>Länken till denna sida:</p>
+  <p><a href="<?php echo getCurrentUrl();?>"><?php echo getCurrentUrl();?></a></p>
   <table>
-    <?php 
-      foreach ($_SERVER as $key => $value) {
-        echo('<tr><td>'. $key . ':</td><td> ' . $value . '</td><tr>'); 
-      }
-    ?>
+  <?php
+    foreach ($_SERVER as $key => $value) {
+    echo('<tr><td>'. $key . ':</td><td> ' . $value . '</td></tr>');
+  }
+  ?>
   </table>
-
- </pre>
