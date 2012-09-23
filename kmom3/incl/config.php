c9dcc9ffce7b576e2a1404adc381d7b761e24100
webjen <jryden@outlook.com>
2012-09-23 17:08:29 +0200

    Include login in config, set title on login multipage

--- kmom3/incl/config.php
+++ kmom3/incl/config.php
@@ -7,3 +7,10 @@ include_once(dirname(__FILE__) . '/../src/common.php');
 
 // Turn on error reporting
 error_reporting(-1);
+
+// include functions for login & logout
+include_once(dirname(__FILE__) . "/../src/login.php");
+
+// account and password that can login
+$userAccount = "doe";
+$userPassword = userPassword("doe");
