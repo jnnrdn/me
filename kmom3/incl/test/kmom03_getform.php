13545a70c7b2c77f6b0563de3e8d7252d6a63e12
webjen <jryden@outlook.com>
2012-09-30 16:34:36 +0200

    Fix error with get form in kmom03

--- kmom3/incl/test/kmom03_getform.php
+++ kmom3/incl/test/kmom03_getform.php
@@ -1,5 +1,5 @@
 <h1>Formulär och get-metoden</h1>
-<form method="get" action="?p=kmom03_getform">
+<form method="get" action="?p=kmom03-getform">
   <fieldset>
     <legend>Exempel på formulär med get-metoden</legend>
     <p>
