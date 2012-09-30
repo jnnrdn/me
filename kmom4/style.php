1cc33789ee09d851dfd31fe0860c10199a8e206b
webjen <jryden@outlook.com>
2012-09-30 12:13:20 +0200

    Changed comments in base_style, inspired by normalize.css removed inline style in style.php and the file new_style.css

--- kmom4/style.php
+++ kmom4/style.php
@@ -32,14 +32,13 @@
 <?php include("incl/header.php"); ?>
 
 <div id="content">
-  <aside class="left" style="width:22%;">
+  <aside class="left">
     <?php include("$path/aside.php"); ?>
   </aside>
-  <article class="right border justify-para" style="width:72%;">
+  <article class="right border justify-para">
     <p class="quiet small">Källkod till denna sida, <code><?php echo "$path/$file"; ?></code>, <a href="viewsource.php?dir=<?php echo $path; ?>&amp;file=<?php echo $file; ?>#file">hittar du här</a>.</p>
     <?php include("$path/$file"); ?>
     <?php include("incl/byline.php"); ?>  
-    <hr>
   </article>
 </div>
 <?php include("incl/footer.php"); ?>
